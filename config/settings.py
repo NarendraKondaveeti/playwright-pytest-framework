3542e8bb709fb8e1ed11a622ecba783ff59408cd
Narendra Kondaveeti <123narendra.k@gmail.com>
2026-07-20 20:56:22 +0530

    headleass dynamic

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Settings:
-    BROWSER = "chromium"
-    HEADLESS = True
-    BASE_URL = "https://the-internet.herokuapp.com/"
-    TIMEOUT = 30000
-    SLOW_MO = 500
+    BROWSER = os.getenv("BROWSER", "chromium")
+    HEADLESS = os.getenv("HEADLESS", "true").lower() == "true"
+    BASE_URL = os.getenv("BASE_URL")
+    TIMEOUT = int(os.getenv("TIMEOUT", "30000"))
+    SLOW_MO = int(os.getenv("SLOW_MO", "500"))
